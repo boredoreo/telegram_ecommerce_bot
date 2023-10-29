4400d1a151bd693b644e6998b3b32778bbd6f7d1
boredoreo <134864203+boredoreo@users.noreply.github.com>
2023-10-29 11:49:21 +0100

    Add files via upload

---
+++ BytesNCrunch2/handlers/browse_products.py
@@ -0,0 +1,110 @@
+import logging, os
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import  CallbackQueryHandler, CommandHandler,  Filters, Updater, ConversationHandler, MessageHandler
+from telegram.files.inputmedia import InputMediaPhoto
+from database.models import  Vendor, Student, Product
+from database.manipulate import load_blob
+from database.query import get_all_vendors, get_my_products
+
+
+'''
+*products should be a list of lists
+*pruducts should be saved in user data either at the point of searching or when being redirected from vendor browsing
+ie products should be fetched prior too getting to this page
+'''
+def browse_product(update,bot):
+    vend = (update.callback_query.data)[6:]
+    products = bot.user_data["products"]
+    product = products[0]
+    file_name = product[1]+".jpg"
+    
+    image = product[2]
+    load_blob(blob=image, file_name=file_name)
+    reply_keyboard = [
+        [
+            InlineKeyboardButton(text="Next", callback_data="browse_product_1")
+        ],
+        [
+            InlineKeyboardButton(text="Add to Cart", callback_data="add_to_cart_{}".format(product[0])),
+        ]
+    ]
+
+    mark_up = InlineKeyboardMarkup(reply_keyboard)
+    
+
+    bot.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo = open(file_name, "rb"),
+        caption= '''
+        \n{}
+        \n{}
+        '''.format(product[2],product[3]),
+        reply_markup = mark_up
+    )
+    os.remove(file_name)
+    
+
+
+def browse_product_extended(update,bot):
+    query = update.callback_query
+    state = int(update.callback_query.data[15:])
+    products_list = bot.user_data["products"]
+    current_product = products_list[state]
+    # vend = my_vendors[state-1]
+    file_name = current_product[1]+".jpg"
+    image = current_product[2]
+    load_blob(blob=image, file_name=file_name)
+
+    if state == len(products_list):
+        reply_keyboard = [
+            [
+                InlineKeyboardButton(text="Previous", callback_data="browse_product_{}".format(state-1))
+            ],
+            [
+            InlineKeyboardButton(text="Add to Cart", callback_data="add_to_cart_{}".format(current_product[0])),
+            ]
+        ]
+    elif state == 1:
+        reply_keyboard = [
+            [
+                InlineKeyboardButton(text="Next", callback_data="browse_product_{}".format(state+1))
+            ],
+            [
+             InlineKeyboardButton(text="Add to Cart", callback_data="add_to_cart_{}".format(current_product[0])),
+            ]
+        ]
+    else:
+        reply_keyboard = [
+            [
+                InlineKeyboardButton(text="Previous", callback_data="browse_product_{}".format(state-1)),
+                InlineKeyboardButton(text="Next", callback_data="browse_product_{}".format(state+1))
+            ],
+            [
+             InlineKeyboardButton(text="Add to Cart", callback_data="add_to_cart_{}".format(current_product[0])),
+            ]
+        ]
+
+
+
+    mark_up = InlineKeyboardMarkup(reply_keyboard)
+    
+
+    query.message.edit_media(
+        InputMediaPhoto(
+            media= open(file_name, "rb"),
+        caption= '''
+        \n{},
+        \n{}
+
+        '''.format(current_product[1],current_product[3])
+            ),
+        
+        reply_markup = mark_up
+    )
+    os.remove(file_name)
+    print(state)
+    return 0
+
+
+browse_product_handler = CallbackQueryHandler(pattern="visit_", callback= browse_product,run_async=True)
+browse_product_ext_handler = CallbackQueryHandler(callback=browse_product_extended, pattern="browse_product_", run_async=True)
---
+++ BytesNCrunch2/handlers/start_up.py
@@ -0,0 +1,175 @@
+from database.models import User, Vendor, Student
+from database.query import (
+    get_user,
+    get_student,
+    get_vendor,
+    is_user,
+    is_student,
+    is_vendor,
+)
+
+from database.manipulate import (
+    commit_user,
+    commit_vendor,
+    commmit_student
+)
+import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    Updater,
+    ConversationHandler,
+    MessageHandler,
+)
+
+
+# start handler
+def start(update, bot):
+    exist = is_user(update.effective_user.id) 
+    if exist:
+        user_type = get_user(update.effective_user.id)
+        role = user_type.role
+        reply_keyboard = [
+            [InlineKeyboardButton(text="Customer Support", callback_data="customer_feedback")],
+            [InlineKeyboardButton(text="Make Order", callback_data="make_order")]
+        ]
+        markup = InlineKeyboardMarkup(reply_keyboard)
+        bot.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="""
+                Welcome to Byte & Crunch! \n How may we be of assistance to you?
+                ***SUBJECT TO CHANGE***
+                """,
+            reply_keyboard=markup
+        )
+        
+    else:
+        reply_keyboard = [
+            [InlineKeyboardButton(text="Let's Go!", callback_data="is_customer")],
+        ]
+        bot.user_data["user"] = User()
+        markup = InlineKeyboardMarkup(reply_keyboard)
+        bot.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="Welcome to Byte & Crunch Telegram Bot!",
+            reply_markup=markup,
+        )
+
+        return 0
+
+
+#is customer
+def setup_customer(update, bot):
+    query = update.callback_query
+    user = bot.user_data["user"]
+    user.userid = update.effective_user.id
+    user.role = "customer"
+    "userid VARCHAR(120) UNIQUE, name VARCHAR(120), matno CHAR(10) UNIQUE, regno CHAR(7) UNIQUE, room VARCHAR(255)"
+    query.edit_message_text(
+            text="""
+                Thank you for chooosing Byte & Crunch!
+                \nWe need some information before you can continue to ordering your food!
+                \nPlease reply with your:
+                \n<name>, <matric number>, and <hall and room number>
+                \nIn that order separated by full stops
+                \neg
+                \nOreo Eniola.19Ck000000.Daniel Hall F208 
+                """
+            )
+    bot.user_data["user"] = user
+    bot.user_data["role"] = user.role
+    # result = update.message.text.split(".")
+    # print(result)
+    print("done")
+
+    return "c_0"
+
+
+def customer_detail(update, bot):
+    user_id = update.effective_user.id
+    role = bot.user_data["role"]
+    result = update.message.text.split(".")
+    print(result)
+    name, matno, room = result
+    
+    bot.user_data["user_type"] = Student(
+        role=role, userid=user_id, name=name, matno=matno, room=room
+    )
+
+    reply_keyboard = [
+        [InlineKeyboardButton(text="Yes, Submit!", callback_data="save")],
+        [InlineKeyboardButton(text="No, Re-enter Details", callback_data="return")],
+    ]
+    mark_up = InlineKeyboardMarkup(reply_keyboard)
+    bot.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"""
+            Here's what you entered: \n
+            Name =>{name}
+            Matric No => {matno}
+            Room details => {room}
+
+            Is this all correct?
+
+            """,
+        reply_markup=mark_up,
+    )
+    return "submit"
+
+
+
+# save user to database
+def submit(update, bot):
+    chat_id = update.effective_chat.id
+    bot.user_data["cart"] = []
+    current_user = bot.user_data["user"]
+    user_type = bot.user_data['user_type']
+    role = bot.user_data["role"]
+    if update.callback_query.data == "save":
+        commit_user(current_user)
+        commmit_student(user_type)        
+        bot.bot.send_message(
+            text="Account Created Successfully! \n /start to view your commands ",
+            chat_id=update.effective_chat.id,
+        )
+
+        del bot.user_data["role"]
+        del bot.user_data['user_type']
+        del bot.user_data["user"]
+        return ConversationHandler.END
+
+        
+    elif update.callback_query.data == "return":
+        bot.bot.send_message(
+            chat_id=update.effective_chat.id,
+               text="""
+                    Please reply with your:
+                    \n<name>, <matric number>, and <hall and room number>
+                    \nIn that order separated by full stops
+                    \neg
+                    \n\nOreo Eniola.19Ck000000.Daniel Hall F208 
+                    """,
+                )
+        return "student"
+
+    return ConversationHandler.END
+
+def cancel(update,bot):
+	update.message.reply_text('Operation canceled')
+	return ConversationHandler.END
+
+
+start_handler = CommandHandler("start", start, run_async=True)
+setup_user_handler = ConversationHandler(
+    entry_points=[
+        CallbackQueryHandler(pattern="is_customer", callback=setup_customer, run_async=True)
+        ],
+    states= {
+        "c_0" : [MessageHandler(Filters.all, customer_detail, run_async=True)],
+        "submit": [CallbackQueryHandler(submit, run_async=True)]
+
+    },
+    fallbacks=[CommandHandler("cancel", cancel)]
+)
