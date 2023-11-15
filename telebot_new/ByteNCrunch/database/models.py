e3f9e8e6e80720cf242215ca729d288d544e6f91
boredoreo <134864203+boredoreo@users.noreply.github.com>
2023-11-15 08:11:55 +0100

    Add files via upload

---
+++ telebot_new/ByteNCrunch/database/models.py
@@ -0,0 +1,122 @@
+from enum import Enum
+
+
+class User:
+    
+    def __init__(self,userid=None, role=None):
+        self.userid=userid
+        self.role=role
+        
+
+        
+class Student:
+
+    def __init__(self, role=None, userid=None, name=None, matno=None, room= None, account_number=None, bank=None):
+        self.role = role
+        self.userid = userid
+        self.name =name
+        self.matno=matno
+        self.room = room
+        self.account_number= account_number
+        self.bank = bank
+
+    def __str__(self):
+        return self.role
+
+    # def load_to_dict(self):
+    #     return {
+    #         "role" : self.role,
+    #         "userid" : self.userid,
+    #         "name":self.name,
+    #         "matno":self.matno,
+    #         "room" : self.room,
+    #         "account_nuumber" : self.account_number,
+    #         "bank": self.bank
+    #     }
+
+class Vendor:    
+    
+    def __init__(
+        self, 
+        vendor_id = None,
+        shop_name =None,
+        account_number=None,
+        bank = None
+        ):
+        self.vendor_id = vendor_id
+        self.shop_name = shop_name
+        self.account_number = account_number 
+        self.bank =  bank
+        self.products = []
+
+    def load_to_dict(self):
+         pass
+    
+    def __str__(self):
+        return {
+             "shop_name":self.shop_name,
+             "account_number":self.account_number,
+             "bank":self.bank,
+             "products":self.products
+        }
+
+class Product:
+    
+    name=None
+    image=None
+    description=None
+    vendorID=None
+    # available=None
+    price=None
+    instance = None
+    variations = None
+    def __init__(
+        self, name=None, 
+        image=None, description=None,
+        vendorID=None, available=None, 
+        price=None, instance=None
+        ):
+        self.name = name
+        self.image = image
+        self.description = description 
+        self.vendorID = vendorID 
+        # self.available = available
+        self.price = price
+        self.instance = instance
+
+
+    def load_to_dict(self):
+        return {
+            "name":self.name,
+            "shop_name":self.shop_name,
+            "email" : self.email,
+            "phone_no" : self.phone_no,
+            "account_nuumber" : self.account_number,
+            "bank": self.bank
+        }
+
+
+
+class Order:
+	def __init__(self, id=None,customer_id=None,status=None):
+		self.id = id
+		self.customer_id= customer_id
+		self.status = status
+		
+		
+class OrderItem:
+	def __init__(self, id=None,product_id=None,order=None,item_count=0):
+		self.id=id
+		self.product_id = product_id
+		self.order=order
+		self.item_count=item_count
+		
+		
+class Status(Enum):
+	Delivered = "delivered"
+	Cancelled = "cancelled"
+	Pending = "pending"
+	Rejected_By_Vendor = "rejected_by_vendor"
+	
+	
+            
---
+++ telebot_new/ByteNCrunch/handlers/browse_products.py
@@ -0,0 +1,90 @@
+# import logging, os
+# from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+# from telegram.ext import  CallbackQueryHandler
+# from database.models import  Vendor, Student, Product
+# from database.query import get_all_vendors, get_products_from
+
+
+# # '''
+# # *products should be a list of lists
+# # *pruducts should be saved in user data either at the point of searching or when being redirected from vendor browsing
+# # ie products should be fetched prior too getting to this page
+# # '''
+# def browse_product(update,bot):
+#     vend = ((update.callback_query.data)[12:])
+#     products = get_products_from(vend)
+#     print(products)
+#     bot.user_data["products"] = products
+#     product = products[0]
+#     reply_keyboard = [
+#         [
+#             InlineKeyboardButton(text="Next", callback_data="browse_product_1")
+#         ],
+#         [
+#             InlineKeyboardButton(text="Add to Cart", callback_data="add_to_cart_{}".format(product[0])),
+#             InlineKeyboardButton(text="Manage Cart", callback_data="manage_cart")
+
+#         ]
+#     ]
+
+#     markup = InlineKeyboardMarkup(reply_keyboard)
+#     bot.bot.send_message(
+#         chat_id=update.effective_chat.id,
+#         text= '''
+#             ###PLACEHOLDER TEXT FOR PRODUCT INFO###
+#             ''',
+#         reply_markup=markup,
+#     )
+
+
+# def browse_product_extended(update,bot):
+#     query = update.callback_query
+#     state = int(update.callback_query.data[15:])
+#     products_list = bot.user_data["products"]
+#     current_product = products_list[state]
+
+#     if state == len(products_list):
+#         reply_keyboard = [
+#             [
+#                 InlineKeyboardButton(text="Previous", callback_data="browse_product_{}".format(state-1))
+#             ],
+#             [
+#                 InlineKeyboardButton(text="Add to Cart", callback_data="add_to_cart_{}".format(current_product[0])),
+#                 InlineKeyboardButton(text="Manage Cart", callback_data="manage_cart")
+
+#             ]
+#         ]
+#     elif state == 1:
+#         reply_keyboard = [
+#             [
+#                 InlineKeyboardButton(text="Next", callback_data="browse_product_{}".format(state+1))
+#             ],
+#             [
+#                 InlineKeyboardButton(text="Add to Cart", callback_data="add_to_cart_{}".format(current_product[0])),
+#                 InlineKeyboardButton(text="Manage Cart", callback_data="manage_cart")
+#             ]
+#         ]
+#     else:
+#         reply_keyboard = [
+#             [
+#                 InlineKeyboardButton(text="Previous", callback_data="browse_product_{}".format(state-1)),
+#                 InlineKeyboardButton(text="Next", callback_data="browse_product_{}".format(state+1))
+#             ],
+#             [
+#                 InlineKeyboardButton(text="Add to Cart", callback_data="add_to_cart_{}".format(current_product[0])),
+#                 InlineKeyboardButton(text="Manage Cart", callback_data="manage_cart")
+#             ]
+#         ]
+
+
+
+#     markup = InlineKeyboardMarkup(reply_keyboard)
+#     query.edit_message_text(
+#         text= current_product[1],
+#         reply_markup=markup,
+#     )
+#     return 0
+
+
+# browse_product_handler = CallbackQueryHandler(pattern="browse_shop_", callback= browse_product,run_async=True)
+# browse_product_ext_handler = CallbackQueryHandler(callback=browse_product_extended, pattern="browse_product_", run_async=True)
