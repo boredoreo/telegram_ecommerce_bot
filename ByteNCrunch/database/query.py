63135a03658bce39aafb6e246c93b12e25a640f0
boredoreo <134864203+boredoreo@users.noreply.github.com>
2023-11-20 16:10:05 +0100

    Update query.py

--- ByteNCrunch/database/query.py
+++ ByteNCrunch/database/query.py
@@ -1,6 +1,6 @@
 import mysql.connector as connector
 from dotenv.main import load_dotenv
-from .models import User, Student
+# from .models import User, Student
 import os
 import config
 
@@ -28,40 +28,7 @@ def is_user(userid = None) -> bool:
     else:
         return True
 
-def get_user(userid) -> User:
 
-    mycon = connector.connect(
-    host= config.DB_HOST,
-    user=config.DB_USER,
-    password=config.DB_PASSWORD,
-    database=config.DATABASE
-    )
-
-    crsr = mycon.cursor()
-    crsr.execute(
-        "SELECT * FROM student WHERE userid=%s",
-        (userid,)
-        )            
-    result = crsr.fetchall()[0]  
-    mycon.close()
-    return User(userid=result[0], role=result[1])
-
-def get_vendor(userid):
-    mycon = connector.connect(
-    host= config.DB_HOST,
-    user=config.DB_USER,
-    password=config.DB_PASSWORD,
-    database=config.DATABASE
-    )
-
-    crsr = mycon.cursor()
-    crsr.execute(
-        "SELECT * FROM vendor WHERE userid=%s",
-        (userid,)
-        )           
-    result = crsr.fetchall()[0]  
-    mycon.close()
-    return 0
 
 def get_all_vendors():
     mycon = connector.connect(
@@ -132,3 +99,18 @@ def get_products_from(myid):
     mycon.close()
     return result
 
+def get_last_order(dets):
+    mycon = connector.connect(
+    host=os.environ["DB_HOST"],
+    user=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWORD"],
+    database=os.environ["DATABASE"]
+    )
+    crsr = mycon.cursor()
+    crsr.execute(
+        "SELECT * FROM orders WHERE customer_id=%s AND customer_name= %s AND ammount_paid=%s",(dets[0],dets[1], dets[2])
+    )
+
+    result = crsr.fetchall()[-1][0]
+    mycon.close()
+    return result
