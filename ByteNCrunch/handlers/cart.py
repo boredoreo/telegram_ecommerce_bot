41d68c8a876ce3793aab86ce95cc04840f998a76
boredoreo <134864203+boredoreo@users.noreply.github.com>
2023-11-29 18:18:47 +0100

    Update cart.py

--- ByteNCrunch/handlers/cart.py
+++ ByteNCrunch/handlers/cart.py
@@ -1,11 +1,13 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import  CallbackQueryHandler
 from database.query import get_product
+from filters.helpers import cart_to_lol
 
 
 def add_to_cart(update, bot):
     query = update.callback_query
     data = update.callback_query.data
+    button = []
     match data[:13]:
         #anything to dodge regex, lol
         case "cart_quan_add":
@@ -15,30 +17,39 @@ def add_to_cart(update, bot):
             product_count = bot.user_data["product_count"] 
             product_count +=1
             bot.user_data["product_count"] = product_count
+            button =  [
+                       [ InlineKeyboardButton(text="-1", callback_data=f"cart_quan_minus_one_{product_id}"),
+                        InlineKeyboardButton(text="+1", callback_data=f"cart_quan_add_one_{product_id}")]
+                    ]
         case "cart_quan_min":
-            if product > 1:
+            if bot.user_data["product_count"] > 1:
                 product_id = update.callback_query.data[20:]
                 product = get_product(product_id)
                 product_count = bot.user_data["product_count"]
                 product_count -=1
                 bot.user_data["product_count"] = product_count
+                button =  [
+                            [InlineKeyboardButton(text="-1", callback_data=f"cart_quan_minus_one_{product_id}"),
+                            InlineKeyboardButton(text="+1", callback_data=f"cart_quan_add_one_{product_id}")]
+                        ]
             else:
                 product_id = update.callback_query.data[20:]
                 product = get_product(product_id)
                 product_count = 1
                 bot.user_data["product_count"] = product_count
+                button =  [[InlineKeyboardButton(text="+1", callback_data=f"cart_quan_add_one_{product_id}")]]
 
         case _:
             product_id = update.callback_query.data[12:]
             product = get_product(product_id)
             product_count = 1
             bot.user_data["product_count"] = product_count
+            button =  [
+                       [InlineKeyboardButton(text="+1", callback_data=f"cart_quan_add_one_{product_id}")]
+                    ]
     
-    reply_keyboard = [
-        [
-            InlineKeyboardButton(text="-1", callback_data=f"cart_quan_minus_one_{product_id}"),
-            InlineKeyboardButton(text="+1", callback_data=f"cart_quan_add_one_{product_id}")
-        ],
+    reply_keyboard = button + [
+       
         [
             InlineKeyboardButton(text="Add to cart", callback_data=f"add_to_cart_{product_id}_{product_count}")
         ],
@@ -57,7 +68,10 @@ def add_to_cart_confirm(update, bot):
     data = update.callback_query.data
     product_id = int(data[12:].split("_")[0])
     product_count = int(data[12:].split("_")[1])
-    bot.user_data["cart"][product_id] = product_count
+    if product_id in bot.user_data["cart"].keys():
+        bot.user_data["cart"][product_id] += product_count    
+    else:
+        bot.user_data["cart"][product_id] = product_count
     bot.user_data["cart_total"] += product_count * get_product(product_id)[3]
     print(get_product(product_id)[3])
     reply_keyboard = [
@@ -68,13 +82,14 @@ def add_to_cart_confirm(update, bot):
          [
             InlineKeyboardButton(text="Manage Cart", callback_data="manage_cart")
         ] ,
+        [InlineKeyboardButton(text="Continue shopping!", callback_data="make_order")],
         [
             InlineKeyboardButton(text="Back to Home!", callback_data="start")
         ]
     ]
     markup = InlineKeyboardMarkup(reply_keyboard)
     query.edit_message_text(
-        text=f"{product_count} units of {get_product(product_id)[1] }added to cart! \n To proceed to checkout, tap on 'Manage Cart'",
+        text=f"{product_count} orders of {get_product(product_id)[1] }added to cart! \n To proceed to checkout, tap on 'Manage Cart'",
         reply_markup=markup,
     )
 
@@ -100,7 +115,7 @@ def manage_cart(update, bot):
         reply_keyboard = [
             
             [
-                InlineKeyboardButton(text="edit", callback_data="edit_cart"),
+                InlineKeyboardButton(text="Edit Cart Itemsw", callback_data="edit_cart"),
                 InlineKeyboardButton(text="Proceed to Checkout", callback_data="checkout")
             ] ,
             [
@@ -113,7 +128,6 @@ def manage_cart(update, bot):
         text=my_text,
         reply_markup=markup,
     )
-
 
 def edit_cart(update, bot):
     query = update.callback_query
@@ -225,11 +239,8 @@ def edit_cart(update, bot):
     )
 
     
-
-    
 add_to_cart_handler = CallbackQueryHandler(callback=add_to_cart, pattern="add_product_", run_async=True )
 cart_quantity_handler = CallbackQueryHandler(callback=add_to_cart, pattern="cart_quan_", run_async=True)
 confirm_cart = CallbackQueryHandler(callback=add_to_cart_confirm, pattern="add_to_cart_")
 manage_cart_handler = CallbackQueryHandler(callback=manage_cart, pattern="manage_cart")
 edit_cart_handler= CallbackQueryHandler(callback=edit_cart, pattern="edit_cart")
-
