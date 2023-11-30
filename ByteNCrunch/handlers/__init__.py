f63040f793dbef1f6c977cf3869e1c4bd134e181
boredoreo <134864203+boredoreo@users.noreply.github.com>
2023-11-30 06:30:42 +0100

    Update __init__.py

--- ByteNCrunch/handlers/__init__.py
+++ ByteNCrunch/handlers/__init__.py
@@ -1,5 +1,6 @@
 # from .cart import add_to_cart_handler
 # from .search import search_handlerfrom .checkout import check_out_handler, direct_transfer_handler, confirm_direct_transfer 
+from .user import edit_room_handler
 from .complaint import complaint_handler
 from .start import start_handler, setup_user_handler, back_to_home
 from .browse_vendors import browse_vendor_handler, browse_vendor_ext_handler
@@ -9,6 +10,9 @@ from .payment import flutterwave_payment_handler
 from .checkout import check_out_handler, direct_transfer_handler,confirm_direct_transfer_handler
 
 all_handlers = [
+    #user handlers
+    edit_room_handler,
+
     # Start up Handlers
     start_handler,
     setup_user_handler,
